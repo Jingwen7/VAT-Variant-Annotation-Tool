77d2e0f214feadc64a9d2cf99906b745bb3d2b5d
jingwen7 <jingwenr@usc.edu>
2022-12-22 19:03:57 -0800

    Add code and example files

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # VAT-Variant-Annotation-Tool
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/github_username/repo_name">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">VAT-Variant-Annotation-Tool</h3>
+
+  <p align="center">
+    VAT is a variant annotation tool that parses VCF files and fetches variant information from Ensembl Variant Effect Predictor (VEP)
+    <br />
+    <a href="https://github.com/github_username/repo_name/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/github_username/repo_name/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [Introduction](#about-the-project)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+	* [Example](#example)
+	* [Output format] (#output)
+* [Contact](#contact)
+
+
+
+<!-- Introduction-->
+## Introduction
+
+VAT is a variant annotation tool that parses VCF files, annotates the following information and output annotation in a tsv file. 
+1. Depth of sequence coverage at the site of variation.
+2. Number of reads supporting the variant.
+3. Percentage of reads supporting the variant versus those supporting reference reads.
+4. Gene of the variant, type of variation (substitution, insertion, deleteion, CNV, etc.) and their effect (missense, silent, intergenic, etc.). The API
+documentation is available here: https://rest.ensembl.org/#VEP
+5. The minor allele frequency of the variant if available.
+6. Additional annotations: rsid, minor_allele_freq, minor_allele, clin_sig, transcript_id, gene_id, impact, gene_symbol, biotype, polyphen_prediction, and sift_prediction. NOTE: there might be multiple transcript_id, gene_id, gene_symbol, polyphen_prediction, sift_prediction for each variant. 
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To install VAT, python (>= 3.10), vcfpy, requests, json5 and jsonschema are required. 
+### Prerequisites
+
+```sh
+conda create --name vat python=3.10
+conda activate vat
+pip install -r requirements.txt
+```
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/Jingwen7/VAT-Variant-Annotation-Tool.git
+```
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+```sh
+usage: vat.py [-h] -i [INPUT_FILE_PATH] -o [OUTPUT_FILE_PATH]
+```
+### Example
+```sh
+python3 vat.py -i /project/mchaisso_100/cmb-16/jingwenr/tempus/test_vcf_data.txt -o /project/mchaisso_100/cmb-16/jingwenr/tempus/out.tsv
+```
+### Output format
+VAT stores the annotation of each variant in a tsv file.
+
+
+The following shows an example of the tsv file
+```
+CHROM	POS	REF	ALT	TYPE	total_read_depth	ref_read_depth	alt_read_depth	Ratio_supporting_reads_alt_vs_ref	rsid	most_severe_consequence	minor_allele_freq	minor_allele	clin_sig	transcript_id	gene_id	gene_symbol	impact	biotype	polyphen_prediction	sift_prediction
+1	11090916	C	A	123	1	122	122.0	COSV68896102	missense_variant	NA	NA	NA	ENST00000400897,ENST00000607145	ENSG00000009724,ENSG00000271895	MASP2,RP4-635E18.8	MODERATE,MODIFIER	protein_coding,antisense	possibly_damaging,NA	deleterious,NA
+1	1650832	A	G	314	163	151	0.9263803680981595	rs72909030	missense_variant	NA	NA	NA	ENST00000356200,ENST00000356937,ENST00000357760,ENST00000358779,ENST00000378633,ENST00000378635,ENST00000378638,ENST00000401096,ENST00000404249,ENST00000460465,ENST00000479362,ENST00000487462,ENST00000498810,ENST00000509982,ENST00000598846	ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000008128,ENSG00000268575	CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,CDK11A,RP1-283E3.8	MODERATE,MODIFIER,MODERATE,MODERATE,MODERATE,MODERATE,MODERATE,MODIFIER,MODERATE,MODERATE,MODERATE,MODIFIER,MODIFIER,MODERATE,MODIFIER	protein_coding,retained_intron,protein_coding,protein_coding,protein_coding,protein_coding,protein_coding,protein_coding,protein_coding,nonsense_mediated_decay,protein_coding,retained_intron,retained_intron,nonsense_mediated_decay,processed_transcript	benign,NA,benign,benign,benign,benign,benign,NA,benign,benign,benign,NA,NA,benign,NA	tolerated_low_confidence,NA,tolerated_low_confidence,tolerated_low_confidence,tolerated_low_confidence,tolerated_low_confidence,tolerated_low_confidence,NA,tolerated_low_confidence,tolerated,tolerated_low_confidence,NA,NA,tolerated,NA
+```
---
+++ vat.py
@@ -0,0 +1,242 @@
+#!~/.conda/envs/lra/bin/python3
+
+"""
+Author: Jingwen Ren (jingwenr@usc.edu)
+PhD Candidate in Epidemiology
+Quantative Biology and Bioinformatics
+University of Southern California
+"""
+
+import argparse
+import os
+import sys
+import csv
+import re
+import pysam 
+from collections import defaultdict
+import vcf
+import requests
+import json
+from requests import HTTPError
+import multiprocessing
+from multiprocessing.dummy import Pool
+
+# Initialize parser
+parser = argparse.ArgumentParser(description="VAT: Variant Annotation Tool")
+parser.add_argument("-i", "--Input_file_path", nargs='?', required=True)
+parser.add_argument("-o", "--Output_file_path", nargs='?', required=True)
+args = parser.parse_args()
+
+
+"""
+Rest client for Ensembl API.
+"""
+class EnsemblRestClient(object):
+    def __init__(self, server="http://grch37.rest.ensembl.org"):
+        self.server = server
+
+    def perform_rest_action(self, endpoint, data=None, params=None):
+        headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
+
+        if params:
+            endpoint += '?' + urlencode(params)
+
+        try:
+            response = requests.post(self.server + endpoint, headers=headers, data=data)
+            content = response.text
+            if content:
+                decode = json.loads(content)
+
+        except HTTPError as e:
+            # check if we are being rate limited by the server
+            if e.code == 429:
+                if 'Retry-After' in e.headers:
+                    retry = e.headers['Retry-After']
+                    time.sleep(float(retry))
+                    self.perform_rest_action(endpoint, data, params)
+            else:
+                sys.stderr.write('Request failed for {0}: Status code: {1.code} Reason: {1.reason}\n'.format(endpoint, e))
+
+        return decode
+    
+    
+    def get_vep(self, var_ids):
+
+        data_dict = {"hgvs_notations" : var_ids}
+        data = json.dumps(data_dict) 
+        response = self.perform_rest_action(endpoint = "/vep/human/hgvs", data = data)
+        if response:
+            return response
+        
+        return None
+
+
+class annotateVariant(object):
+    def __init__(self, vcf_path):
+        self.vcf_reader = vcf.Reader(open(vcf_path, 'r'))
+        self.annotate_res = defaultdict(dict) # cpra_id -> {"TC": , "TR": , }
+        self.basicInfo()
+    
+    """
+    get a unique cpra id to the INFO col of each variant 
+    """
+    def get_cpra_ID(self, record):
+        alt_str = ','.join([str(a) for a in record.ALT])
+        cpra_id = str(record.CHROM) + ":g." + str(record.POS) + record.REF + ">" + alt_str
+        return cpra_id
+    
+    def basicInfo(self):
+        for record in self.vcf_reader:
+            if record.FILTER:
+                continue 
+            
+            if record.INFO["TC"] < sum(record.INFO["TR"]):
+                continue
+                
+            cpra_id = self.get_cpra_ID(record)
+            self.annotate_res[cpra_id]["CHROM"] = record.CHROM
+            self.annotate_res[cpra_id]["POS"] = record.POS
+            self.annotate_res[cpra_id]["REF"] = record.REF
+            self.annotate_res[cpra_id]["ALT"] = record.ALT
+            self.annotate_res[cpra_id]["TC"] = record.INFO["TC"]
+            self.annotate_res[cpra_id]["TR"] = sum(record.INFO["TR"])
+            self.annotate_res[cpra_id]["GT"] = record.samples[0]['GT']
+        return 
+    
+    """
+    decide TYPE of variant
+    """
+    def svType(self):
+        for var in self.annotate_res:
+
+            numAlt = len(self.annotate_res[var]["ALT"])
+            types = []
+            for i in range(numAlt):
+                if len(self.annotate_res[var]["REF"]) == len(self.annotate_res[var]["ALT"][i]) == 1:
+                    types.append("snp")
+                elif len(self.annotate_res[var]["REF"]) > len(self.annotate_res[var]["ALT"][i]):
+                    types.append("del")
+
+                else:
+                    types.append("ins")
+
+            self.annotate_res[var]["TYPE"] = types
+        
+        return 
+        
+  
+    """
+    calculate # of reads supporting ALT variants / # of reads supporting REF allele
+    assumption:  # of reads supporting REF allele = total reads - # of reads supporting ALT variants
+    NOTE: filter out variant where TR of ALT alleles > TC
+    """
+    def percentReadsSupportALTvsRef(self):
+        for var in self.annotate_res:
+            try:
+                tc = self.annotate_res[var]["TC"]
+                tr = self.annotate_res[var]["TR"]
+                if tc > tr:
+                    self.annotate_res[var]["TF"] = tc - tr
+                    self.annotate_res[var]["PRAR"] = tr / (tc - tr) 
+                elif tc == tr:
+                    self.annotate_res[var]["TF"] = 0
+                    self.annotate_res[var]["PRAR"] = 'INF'
+            except KeyError:
+                print("KeyError")      
+
+    
+    def task(self, idx):
+        var_list = self.var_chunks[idx]
+        client = EnsemblRestClient()
+        var_response_list = client.get_vep(var_list)
+        return var_response_list
+        
+            
+    """
+    Fetch annotation from VEP API
+    """
+    def fetchVarInFoFromVEP(self, numOfProcessors=min(multiprocessing.cpu_count() - 1, 15)):
+
+        numQueryPerTime = 200
+        input_var_list = list(self.annotate_res.keys())
+        self.var_chunks = [input_var_list[i:i + numQueryPerTime] for i in range(0, len(input_var_list), numQueryPerTime)]
+        chunks = range(len(self.var_chunks))
+        numOfProcessors = min(numOfProcessors, len(chunks))
+
+        print("start fetching VEP API with %s processors...."%numOfProcessors)
+        pool = Pool(numOfProcessors)
+        # try:
+        for response in pool.imap_unordered(self.task, chunks):
+            
+            for i in range(len(response)):
+                var_id = response[i]['input']
+                self.annotate_res[var_id]["ANNO"] = defaultdict(lambda: 'NA')
+                if 'transcript_consequences' not in response[i]:
+                    continue 
+                numTranscript = len(response[i]['transcript_consequences'])
+                av.annotate_res[var_id]['ANNO']['transcript_id'] = ",".join([response[i]['transcript_consequences'][j].get('transcript_id', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']['gene_id'] = ",".join([response[i]['transcript_consequences'][j].get('gene_id', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']['gene_symbol'] = ",".join([response[i]['transcript_consequences'][j].get('gene_symbol', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["biotype"] = ",".join([response[i]['transcript_consequences'][j].get('biotype', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["impact"] = ",".join([response[i]['transcript_consequences'][j].get('impact', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["gene_symbol"] = ",".join([response[i]['transcript_consequences'][j].get('gene_symbol', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["polyphen_prediction"] = ",".join([response[i]['transcript_consequences'][j].get('polyphen_prediction', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["sift_prediction"] = ",".join([response[i]['transcript_consequences'][j].get('sift_prediction', 'NA') for j in range(numTranscript)])
+                av.annotate_res[var_id]['ANNO']["most_severe_consequence"] = response[i].get('most_severe_consequence', 'NA')
+
+                if 'colocated_variants' not in response[i]:
+                    continue
+                if len(response[i]['colocated_variants']) == 1:
+                    av.annotate_res[var_id]['ANNO']["minor_allele"] = response[i]['colocated_variants'][0].get('minor_allele', 'NA')
+                    av.annotate_res[var_id]['ANNO']["minor_allele_freq"] = response[i]['colocated_variants'][0].get('minor_allele_freq', 'NA')
+                    av.annotate_res[var_id]['ANNO']["rsid"] = response[i]['colocated_variants'][0].get('id', 'NA')  
+                    av.annotate_res[var_id]['ANNO']["clin_sig"] = response[i]['colocated_variants'][0].get('clin_sig', 'NA')
+
+                else:
+                    av.annotate_res[var_id]['ANNO']["minor_allele"] = response[i]['colocated_variants'][1].get('minor_allele', 'NA')
+                    av.annotate_res[var_id]['ANNO']["minor_allele_freq"] = response[i]['colocated_variants'][1].get('minor_allele_freq', 'NA')
+                    av.annotate_res[var_id]['ANNO']["rsid"] = response[i]['colocated_variants'][1].get('id', 'NA')
+                    av.annotate_res[var_id]['ANNO']["clin_sig"] = response[i]['colocated_variants'][1].get('clin_sig', 'NA')
+        # except:
+        #     print ("Outer exception caught!")
+
+        pool.close()
+        pool.join()
+        print("finish fetching VEP API....")
+        return 
+    
+    """
+    Write output tsv file
+    """
+    def out(self, out_file):
+        with open(out_file, 'w', newline='') as tsvfile:
+            writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
+            writer.writerow(["CHROM", "POS", "REF", "ALT", "TYPE", "total_read_depth", \
+                             "ref_read_depth", "alt_read_depth", "Ratio_supporting_reads_alt_vs_ref", \
+                              "rsid", "most_severe_consequence", "minor_allele_freq", \
+                             "minor_allele", "clin_sig", "transcript_id", "gene_id", "gene_symbol", "impact", \
+                             "biotype", "polyphen_prediction", "sift_prediction"])
+            
+            for var, info in self.annotate_res.items():
+                if 'ANNO' in info and info['ANNO']:
+                    writer.writerow([info['CHROM'], info['POS'], info['REF'], ','.join([str(ch) for ch in info["ALT"]]), str(info['TC']), \
+                                     str(info['TF']), str(info['TR']), str(info['PRAR']), \
+                                     info['ANNO']['rsid'], info['ANNO']['most_severe_consequence'], str(info['ANNO']['minor_allele_freq']), \
+                                     info['ANNO']['minor_allele'], str(info['ANNO']['clin_sig']), info['ANNO']['transcript_id'], \
+                                     info['ANNO']['gene_id'], info['ANNO']['gene_symbol'], info['ANNO']['impact'], \
+                                     info['ANNO']['biotype'], info['ANNO']['polyphen_prediction'], info['ANNO']['sift_prediction'],\
+                                    ])
+                else:
+                    writer.writerow([info['CHROM'], info['POS'], info['REF'], ','.join([str(ch) for ch in info["ALT"]]), str(info['TC']), \
+                                     str(info['TF']), str(info['TR']), str(info['PRAR'])] + ['NA'] * 12)
+        return 
+
+
+
+if  __name__ == "__main__":
+    av = annotateVariant(args.Input_file_path)
+    av.svType()
+    av.percentReadsSupportALTvsRef()
+    av.fetchVarInFoFromVEP()
+    av.out(args.Output_file_path)
+                     
